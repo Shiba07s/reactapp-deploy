5c9e583f885fdfaf6f3049ced711a9aee97a1022
ldtech-weighbridge <ashutosh.samantaray@ldtech.in>
2024-05-09 11:20:54 +0530

    afafa

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={"/my-app"}>
-    	<Route path="/" component={withRouter(App)} />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+)
